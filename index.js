f5210f8cb2fabfa84b4fb20c254a2994628d8a8c
thevan96 <vothevan96@gmail.com>
2020-08-14 10:16:12 +0700

    Setup vanila firebase notification

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import messaging from '@react-native-firebase/messaging';
 
+import {name as appName} from './app.json';
+import App from './App';
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handle running handle on background', remoteMessage);
+});
 AppRegistry.registerComponent(appName, () => App);
